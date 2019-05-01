bba834e39ff933229990f4143104222069d574ac
Krasikov <krasikovap@gmail.com>
2019-05-02 02:34:33 +0600

    xml parsing

--- src/service/functions/list/handlers/handler.go
+++ src/service/functions/list/handlers/handler.go
@@ -1,11 +1,31 @@
 package handlers
 
 import (
+	"io/ioutil"
 	"net/http"
+	"service/common/server"
+
+	"encoding/xml"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Handle(c *gin.Context) {
+	var req server.SingleDataRequest
+
+	if err := c.ShouldBind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+
+	file, _ := req.Data.Open()
+	content, _ := ioutil.ReadAll(file)
+
+	var data server.AirFareSearchResponse
+	if err := xml.Unmarshal(content, &data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"success": false, "error": err.Error()})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"success": true, "message": "list handler"})
 }
